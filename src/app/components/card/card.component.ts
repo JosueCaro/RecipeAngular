cffd88cf6ccb0258176a01038090f7f4b0cab43b
Josué Caro <josuecaro@MacBook-Pro-de-Josue.local>
2023-06-06 13:17:52 -0600

    all

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IRecipe } from 'src/app/interfaces/recipe.interface';
 
 @Component({
   selector: 'app-card',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent {
-
+  @Input() recipe!: IRecipe;
 }
