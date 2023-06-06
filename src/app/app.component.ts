a19549250ebaa389ed6283d09aaf6c85baf2d694
Josué Caro <josuecaro@MacBook-Pro-de-Josue.local>
2023-06-06 12:39:12 -0600

    App Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators }
 from '@angular/forms';
 import { RecipeService } from './services/recipe.service';
+import { IRecipe } from './interfaces/recipe.interface';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +11,7 @@ import { RecipeService } from './services/recipe.service';
 })
 export class AppComponent {
 
+  recipes: IRecipe[] = [];
   isLoading: boolean = false;
   form: FormGroup = new FormGroup({
     ingredients: new FormControl('', [
@@ -17,17 +19,18 @@ export class AppComponent {
       Validators.minLength(10)
     ])
   });
-
-  constructor(private recipeService: RecipeService) {}
-
-
-
-
-  validar() {
+  constructor(private recipeService: RecipeService) {
+    this.recipeService.getRecipes().then(recipes => this.recipes = recipes);
+  }
+  async validar() {
     this.form.controls['ingredients'].markAsDirty();
     if (this.form.valid) {
       this.form.controls['ingredients'].disable();
       this.isLoading = true;
+      await this.recipeService.getRecipe(this.form.controls['ingredients'].value);
+      await this.recipeService.getRecipes().then(recipes => this.recipes = recipes);
+      this.form.controls['ingredients'].enable();
+      this.isLoading = false;
     }
   }
 
